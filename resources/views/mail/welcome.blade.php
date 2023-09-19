4dab23104dcbad4761118d8e554130dd7783f1d0
Intuneteq <tobiolanitori@gmail.com>
2023-09-19 20:32:07 +0100

    fix:register event bug fix

--- resources/views/mail/welcome.blade.php
+++ resources/views/mail/welcome.blade.php
@@ -1,7 +1,8 @@
 <x-mail::message>
+    <x-slot name="imageUrl">https://productize.nyc3.cdn.digitaloceanspaces.com/productize/welcome-to-productize.png</x-slot>
     <h1>Welcome to PRODUCTIZE!</h1>
     <p>Lorem ipsum dolor sit amet consectetur. Habitant aliquet suscipit sed facilisi sit. Nibh at nisl augue viverra vitae amet orci lorem. Luctus faucibus laoreet eu parturient in. Elementum consectetur enim fames velit sit donec.</p>
-    <x-mail::button url="https://intuneteq.tk" color="#6D5DD3" >
+    <x-mail::button url="https://intuneteq.tk" color="#6D5DD3">
         Complete your Profiile
     </x-mail::button>
 </x-mail::message>

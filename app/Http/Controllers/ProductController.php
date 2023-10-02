0d3acb4bcc9de072df65b320d3ea79116b75ee6b
Intuneteq <tobiolanitori@gmail.com>
2023-10-02 08:07:46 +0100

    feat: refactored product controller using repository pattern

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,23 +6,22 @@ use App\Models\Product;
 use App\Http\Requests\StoreProductRequest;
 use App\Http\Requests\UpdateProductRequest;
 use App\Http\Resources\ProductResource;
+use App\Repositories\ProductRepository;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\File;
-use Illuminate\Support\Facades\Storage;
 
 class ProductController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    public function __construct(
+        protected ProductRepository $productRepository
+    ) {
+    }
+
     public function index()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+
     public function store(StoreProductRequest $request)
     {
         $user = Auth::user();
@@ -41,69 +40,29 @@ class ProductController extends Controller
         unset($validated['cover_photos']);
         unset($validated['thumbnail']);
 
-
-        /**
-         * I need file limit
-         * I need file formats
-         */
-        $uploadedData = [];
-        foreach ($data as $item => $file) {
-            $originalName = str_replace(' ', '', $file->getClientOriginalName());
-
-            $path = Storage::putFileAs('digital-products', $file, $originalName);
-
-            $url = config('filesystems.disks.spaces.cdn_endpoint') . '/' . $path;
-
-            array_push($uploadedData, $url);
-        }
-
-        $uploadedCoverPhotos = [];
-        foreach ($cover_photos as $item => $file) {
-            $originalName = str_replace(' ', '', $file->getClientOriginalName());
-
-            $path = Storage::putFileAs('products-cover-photos', $file, $originalName);
-
-            $url = config('filesystems.disks.spaces.cdn_endpoint') . '/' . $path;
-
-            array_push($uploadedCoverPhotos, $url);
-        }
-
-        $thumbnailPath = Storage::putFileAs(
-            'products-thumbnail',
+        $product = $this->productRepository->create(
+            $validated,
             $thumbnail,
-            str_replace(' ', '', $thumbnail->getClientOriginalName())
+            $data, // The digital products
+            $cover_photos
         );
-
-        $thumbnail = config('filesystems.disks.spaces.cdn_endpoint') . '/' . $thumbnailPath;
-
-        $validated['data'] = $uploadedData;
-        $validated['cover_photos'] = $uploadedCoverPhotos;
-        $validated['thumbnail'] = $thumbnail;
-
-        $product = Product::create($validated);
 
         return new ProductResource($product);
     }
 
-    /**
-     * Display the specified resource.
-     */
+
     public function show(Product $product)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
+
     public function update(UpdateProductRequest $request, Product $product)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
+
     public function destroy(Product $product)
     {
         //
--- app/Repositories/ProductRepository.php
+++ app/Repositories/ProductRepository.php
@@ -3,11 +3,66 @@
 namespace App\Repositories;
 
 use App\Models\Product;
+use Illuminate\Support\Facades\Storage;
 
 class ProductRepository
 {
-    public function create(array $credentials)
+    public function create(array $credentials, mixed $thumbnail, mixed $data, mixed $cover_photos)
     {
+        $thumbnail = $this->uploadThumbnail($thumbnail);
+
+        $cover_photos = $this->uploadCoverPhoto($cover_photos);
+
+        $data = $this->uploadData($data);
+
+        $credentials['data'] = $data;
+        $credentials['cover_photos'] = $cover_photos;
+        $credentials['thumbnail'] = $thumbnail;
+
         return Product::create($credentials);
+    }
+
+    private function uploadData(mixed $data)
+    {
+        $uploadedData = [];
+
+        foreach ($data as $item => $file) {
+            $originalName = str_replace(' ', '', $file->getClientOriginalName());
+
+            $path = Storage::putFileAs('digital-products', $file, $originalName);
+
+            $url = config('filesystems.disks.spaces.cdn_endpoint') . '/' . $path;
+
+            array_push($uploadedData, $url);
+        }
+
+        return $uploadedData;
+    }
+
+    private function uploadCoverPhoto(mixed $cover_photos)
+    {
+        $uploadedCoverPhotos = [];
+        foreach ($cover_photos as $item => $file) {
+            $originalName = str_replace(' ', '', $file->getClientOriginalName());
+
+            $path = Storage::putFileAs('products-cover-photos', $file, $originalName);
+
+            $url = config('filesystems.disks.spaces.cdn_endpoint') . '/' . $path;
+
+            array_push($uploadedCoverPhotos, $url);
+        }
+
+        return $uploadedCoverPhotos;
+    }
+
+    private function uploadThumbnail($thumbnail)
+    {
+        $thumbnailPath = Storage::putFileAs(
+            'products-thumbnail',
+            $thumbnail,
+            str_replace(' ', '', $thumbnail->getClientOriginalName())
+        );
+
+        return config('filesystems.disks.spaces.cdn_endpoint') . '/' . $thumbnailPath;
     }
 }

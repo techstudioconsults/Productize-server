4bb47a09611406db7b20fa7a4bed0e3047b4549d
Intuneteq <tobiolanitori@gmail.com>
2023-10-21 12:07:06 +0100

    chore: implemented database seeders for products and users

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Database\Seeders\Traits\DisableForeignKeys;
+use Database\Seeders\Traits\TruncateTable;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    use TruncateTable, DisableForeignKeys;
+
+    public function run(): void
+    {
+        $this->disableForeignKeys();
+        $this->truncate('users');
+        User::factory(2)->create();
+        $this->enableForeignKeys();
+    }
+}

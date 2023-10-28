aa29c1e3510ac91540e4ece5fa06597f7e92a200
Intuneteq <tobiolanitori@gmail.com>
2023-10-28 15:56:43 +0100

    chore: clean up

--- tests/Feature/v1/repository/ProductTest.php
+++ tests/Feature/v1/repository/ProductTest.php
@@ -11,10 +11,10 @@ class ProductTest extends TestCase
     /**
      * A basic feature test example.
      */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    // public function test_example(): void
+    // {
+    //     $response = $this->get('/');
 
-        $response->assertStatus(200);
-    }
+    //     // $response->assertStatus(200);
+    // }
 }

bdc472914d190cb14460e5e229981d0920911f1c
Intuneteq <tobiolanitori@gmail.com>
2023-12-11 03:13:04 +0000

    feat: first product created property implemented

--- app/Listeners/SendProductCreatedMail.php
+++ app/Listeners/SendProductCreatedMail.php
@@ -5,6 +5,7 @@ namespace App\Listeners;
 use App\Events\Products;
 use App\Mail\FirstProductCreated;
 use App\Mail\ProductCreated;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Mail;
 
@@ -32,6 +33,10 @@ class SendProductCreatedMail
             // Send product created mail
             Mail::to($user)->send(new ProductCreated($event->product));
         } else {
+            // Update first product created at property
+            $user->first_product_created_at = Carbon::now();
+            $user->save();
+
             // Send first product created mail
             Mail::to($user)->send(new FirstProductCreated($event->product));
         };

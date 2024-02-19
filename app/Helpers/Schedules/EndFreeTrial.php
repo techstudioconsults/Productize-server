3584aa11a2373d3c66d55052cf7ced8f2d399b1f
Intuneteq <tobiolanitori@gmail.com>
2024-02-19 05:11:39 +0000

    feat: implemented free trial update to free after 30 days

--- app/Helpers/Schedules/EndFreeTrial.php
+++ app/Helpers/Schedules/EndFreeTrial.php
@@ -2,6 +2,8 @@
 
 namespace App\Helpers\Schedules;
 
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
 class EndFreeTrial
@@ -9,6 +11,22 @@ class EndFreeTrial
     public function __invoke()
     {
         Log::channel('schedule')->info('Ending Free Trials!');
-        // DB::table('myTable')->where('x', '>', 0)->update(['x' => 0]);
+
+        // Get the current date
+        $currentDate = Carbon::now();
+
+        // Calculate the date 30 days ago
+        // $thirtyDaysAgo = $currentDate->subDays(30);
+        $thirtyDaysAgo = $currentDate->subMinute(30);
+
+        try {
+            $count = DB::table('users')->where('account_type', '=', 'free_trial')
+                ->where('created_at', '<', $thirtyDaysAgo)
+                ->update(['account_type' => 'free']);
+
+            Log::channel('schedule')->info('Total Update Count', ['count' => $count]);
+        } catch (\Throwable $th) {
+            Log::channel('schedule')->error('Error Ending Free Trials!');
+        }
     }
 }

b726a24fbd636c346e94bf4e994c82c70e813886
Intuneteq <tobiolanitori@gmail.com>
2024-06-14 13:01:00 +0100

    test: payout repository test done

--- database/factories/AccountFactory.php
+++ database/factories/AccountFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -22,6 +23,7 @@ class AccountFactory extends Factory
             'name' => fake()->name(),
             'bank_code' => "033",
             'bank_name' => "United Bank of Africa",
+            'user_id' => User::factory()->create()->id
         ];
     }
 }

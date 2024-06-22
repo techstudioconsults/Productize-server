48862dd28bff1c04370028caf3774ef37abb262f
Intuneteq <tobiolanitori@gmail.com>
2024-06-22 09:47:03 +0100

    feat: implementing revenues

---
+++ database/migrations/2024_06_22_080549_create_revenues_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Enums\RevenueActivity;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('revenues', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignIdFor(\App\Models\User::class, 'user_id');
+            $table->enum('activity', array_column(RevenueActivity::cases(), 'value'));
+            $table->text('product');
+            $table->integer('amount')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('revenues');
+    }
+};

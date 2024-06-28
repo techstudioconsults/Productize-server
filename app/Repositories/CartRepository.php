0488b27716c1e030a1d6bde8154a6744ecbba61e
obajide028 <obajide028@gmail.com>
2024-06-28 22:02:53 +0100

    update

--- app/Repositories/CartRepository.php
+++ app/Repositories/CartRepository.php
@@ -137,7 +137,7 @@ class CartRepository extends Repository
     public function calculateTotalAmount(array $products)
     {
         $total = array_reduce($products, function ($total_amount, $product) {
-            return $total_amount + $product['total_amount'];
+            return $total_amount + $product['amount'];
         }, 0);
     
         return round($total, 2); // Round to 2 decimal places

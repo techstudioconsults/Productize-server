af4c35e1dcb07a3a194c59201fb902e34620fcfa
Intuneteq <tobiolanitori@gmail.com>
2024-07-01 19:25:26 +0100

    fix: minor fixes

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -72,21 +72,6 @@ class SubscriptionController extends Controller
      */
     public function store(StoreSubscriptionRequest $request)
     {
-        // check if user is subscriped on the db table
-        // if yes, return error - no point continuing.
-        // check if we have the user on the subscription table - First time subscriber or db wiped in dev ?
-        // If we have the subscriber on the db table, return error - user is a subscriber but it's cancelled. Let them use the enable button.
-
-        // No user on subscription table
-        // fetch user on paystack
-        // if user is found, check if user has any subscription history
-
-        // If yes, it should be on the db table - Most likely a dev wipe,
-        // check the status, if it is not cancelled, upgrade the user to premium - when moving to production, add the production condition to it so this behavior is not persistent in production
-        // so update the table with necessary subscription info log and throw error.
-
-        //if no user found or no subscription, create a new subscription
-
         // Check if the user is authenticated
         $user = $request->user('sanctum');
 
--- resources/views/mail/email-verification.blade.php
+++ resources/views/mail/email-verification.blade.php
@@ -3,9 +3,15 @@
     <h1>Hi {{ $name }}, Welcome To Productize.
         Verify Your Email Address.</h1>
 
-    <p>Lorem ipsum dolor sit amet consectetur. Habitant aliquet suscipit sed facilisi sit. Nibh at nisl augue viverra vitae amet orci lorem. Luctus faucibus laoreet eu parturient in. Elementum consectetur enim fames velit sit donec.</p>
+    <p>Welcome to Productize, the ultimate digital marketplace for creators like you! We're thrilled to have you join our vibrant community and showcase your digital creations to the world.But before we embark on this exciting journey together, we need to verify your email address to ensure the security of your account and the authenticity of your creations.</p>
+
+    <p>To confirm your email address and get started, please click on the verification button below:</p>
 
     <x-mail::button :url="trim($url)" color="success">
         Verify Email
     </x-mail::button>
+
+    <p>By confirming your email, you'll gain full access to all the features and opportunities our platform offers, including selling your digital products, connecting with buyers, and becoming part of a creative community.</p>
+
+    <p>Thank you for choosing Productize to share your talent and digital creations. We can't wait to see what you have in store for the world!</p>
 </x-mail::message>

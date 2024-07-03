163bdf0ac94b94652edbe6c5ea3b9cdc84ac1f42
Intuneteq <tobiolanitori@gmail.com>
2024-07-03 18:38:51 +0100

    chore: updated rest password email template content

--- resources/views/mail/reset-password.blade.php
+++ resources/views/mail/reset-password.blade.php
@@ -3,11 +3,27 @@
 
     <h1>Reset Password</h1>
 
-    <p>You are receiving this email because we received a password reset request for your account.</p>
+    <p>
+        Please click on the button below for your password change. If you initiated this change, please proceed with the change using the button below.
+    </p>
 
     <small>This password reset link will expire in {{ $count }} minutes. If you did not request a password reset, no further action is required.</small>
 
     <x-mail::button :url="trim($url)" color="#6D5DD3">
         Reset Password
     </x-mail::button>
+
+    <p>
+        However, if you did not request this password reset, please contact our support team immediately at [Support Email Address]. Your account's security is our top priority, and we'll assist you in taking any necessary steps to ensure its safety.
+    </p>
+
+    <p>
+        Thank you for being a part of Productize. If you have any further questions or concerns, please don't hesitate to reach out to us.
+    </p>
+
+    <p class="text-left">
+        Best regards,
+        <br>
+        The Productize Team"
+    </p>
 </x-mail::message>

fbb5fb6488f11df0d9ecbfa9adcc79bd4c9da69b
Intuneteq <Intuneteq@users.noreply.github.com>
2024-07-04 13:23:44 +0000

    Fixes coding style

--- app/Dtos/InvoiceDto.php
+++ app/Dtos/InvoiceDto.php
@@ -29,8 +29,7 @@ class InvoiceDto implements IDtoFactory
         private string $reference,
         private string $description,
         private string $createdAt,
-    ) {
-    }
+    ) {}
 
     /**
      * Get the invoice amount.
@@ -94,14 +93,14 @@ class InvoiceDto implements IDtoFactory
      */
     public static function create(array $data): self
     {
-        if (!isset($data['amount'], $data['status'], $data['createdAt'])) {
+        if (! isset($data['amount'], $data['status'], $data['createdAt'])) {
             throw new ServerErrorException('Invalid Invoice Data Transfer');
         }
 
         return new self(
             $data['amount'],
             $data['status'],
-            $data['reference'] ?? "",
+            $data['reference'] ?? '',
             $data['description'] ?? '',
             $data['createdAt'],
         );

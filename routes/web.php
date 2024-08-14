1b7a79d5504d97c46ff282abbc24394f1b3cf1d0
Intuneteq <Intuneteq@users.noreply.github.com>
2024-08-14 00:58:55 +0000

    Fixes coding style

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     $environment = env('APP_ENV', 'production');
+
     return view('welcome', compact('environment'));
 });
 

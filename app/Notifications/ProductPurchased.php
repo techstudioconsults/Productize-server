8dc0e4cb903e92709092babe0537810916864373
Intuneteq <tobiolanitori@gmail.com>
2025-04-04 10:07:32 +0100

    feat: paystack redirect update

--- app/Notifications/ProductPurchased.php
+++ app/Notifications/ProductPurchased.php
@@ -63,17 +63,17 @@ class ProductPurchased extends Notification implements ShouldQueue
         if ($this->funnel != null) {
             return (new MailMessage)
                 ->markdown('mail.product-purchased-via-funnel', [
-                    'url' => config('app.client_url').'/dashboard/downloads#all-downloads',
+                    'url' => config('app.client_url') . '/dashboard/' . $notifiable->id . '/downloads',
                     'title' => $this->product->title,
                     'thumbnail' => $this->product->thumbnail,
-                    'button' => config('app.client_url').'/products/'.$this->product->slug,
+                    'button' => config('app.client_url') . '/products/' . $this->product->slug,
                 ])
                 ->subject('Your Package is here!')->attach('/funnels-asset/CoursePic3.png');
         }
 
         return (new MailMessage)
             ->markdown('mail.product-purchased', [
-                'url' => config('app.client_url').'/dashboard/downloads#all-downloads',
+                'url' => config('app.client_url') . '/dashboard/' . $notifiable->id . '/downloads',
                 'title' => $this->product->title,
             ])
             ->subject('Product Purchased');
